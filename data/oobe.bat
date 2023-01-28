5693558b5a07b32bf9bdd080c8d39e936418e183
joebobbio <profjstark2@gmail.com>
2023-01-27 22:02:44 -0500

    Fixes and updates
    * Fix Oculus app
    * WiFi connections can now be made normally
    * Storage saving enabled by default
    * Documentation fixes

--- data/oobe.bat
+++ data/oobe.bat
@@ -10,6 +10,8 @@ net localgroup /add administrators MyUserName
 reg add "HKLM\SYSTEM\Setup" /v "SystemSetupInProgress" /t REG_DWORD /d "0" /f
 reg add "HKLM\SYSTEM\Setup" /v "SetupType" /t REG_DWORD /d "0" /f
 reg add "HKLM\SYSTEM\Setup" /v "OOBEInProgress" /t REG_DWORD /d "0" /f
+powercfg -h off
+compact /compactos:always
 echo All done! Your system will reboot momentarily.
 echo (do not press CTRL + C to stop)
 timeout /t 10 /NOBREAK
